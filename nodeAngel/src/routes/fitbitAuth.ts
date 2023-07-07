cb2d0dda9537477728f07053c8eef08168108ce7
Guillotex14 <80910044+Guillotex14@users.noreply.github.com>
2023-07-07 17:31:53 -0400

    Update fitbitAuth.ts

--- nodeAngel/src/routes/fitbitAuth.ts
+++ nodeAngel/src/routes/fitbitAuth.ts
@@ -31,66 +31,6 @@ const authenticate = async (code: string) => {
     accessToken = response.data.access_token;
     refreshToken = response.data.refresh_token;
 
-    
-        console.log('Access token:', accessToken);
-        console.log('Refresh token:', refreshToken);
-    } catch (error) {
-        //console.error('Error al autenticar:', error.response.data);
-    }
-};
-
-const refreshTokens = async () => {
-if (!refreshToken) {
-    console.log('No hay token de refresco');
-    return;
-}
-
-const params = {
-    grant_type: 'refresh_token',
-    refresh_token: refreshToken,
-};
-
-const headers = {
-    'Content-Type': 'application/x-www-form-urlencoded',
-    Authorization: `Basic ${Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
-};
-
-try {
-    const response = await axios.post('https://api.fitbit.com/oauth2/token', querystring.stringify(params), {
-    headers: headers,
-    });
-
-    accessToken = response.data.access_token;
-    refreshToken = response.data.refresh_token;
-
-    console.log('Nuevo access token:', accessToken);
-    console.log('Nuevo refresh token:', refreshToken);
-} catch (error) {
-    //console.error('Error al renovar los tokens:', error.response.data);
-}
-};
-
-authFitbit.get('/profile', async (req: Request, res: Response) => {
-    try {
-        if (!accessToken) {
-        console.log('No hay token de acceso');
-        res.sendStatus(401);
-        return;
-        }
-
-        const response = await axios.get('https://api.fitbit.com/1/user/-/profile.json', {
-        headers: {
-            Authorization: `Bearer ${accessToken}`,
-        },
-        });
-
-        const data = response.data;
-        res.json(data);
-    } catch (error) {
-        //console.error('Error al obtener los datos de perfil:', error.response.data);
-        res.sendStatus(500);
-    }
-  
     console.log('Access token:', accessToken);
     console.log('Refresh token:', refreshToken);
   } catch (error) {
@@ -149,7 +89,6 @@ authFitbit.get('/profile', async (req: Request, res: Response) => {
     console.error('Error al obtener los datos de perfil:');
     res.sendStatus(500);
   }
-
 });
 
 // Renovar automáticamente el token cada 24 horas
